588a82bff97d503b31c15e5b09094d4a02273e3d
Rasmus Borgsmidt <rasmus@borgsmidt.dk>
2018-03-08 11:21:08 +0100

    implement ipfsHash(string) which creates an IPFS multihash without writing to IPFS

---
+++ index.js
@@ -0,0 +1,5 @@
+"use strict";
+
+module.exports = {
+    ipfsHash: require('./lib/ipfs-hash')
+};
---
+++ test/ipfs-hash-test.js
@@ -0,0 +1,26 @@
+"use strict";
+
+var chai = require('chai');
+chai.use(require('chai-as-promised'));
+var expect = chai.expect;
+var fs = require('fs');
+var ipfsHash = require('../lib/ipfs-hash');
+
+describe('ipfsHash', function() {
+    before(setUp);
+    it('should generate IPFS multihash of small data', function(done) {
+        expect(ipfsHash(this.small)).to.eventually.equal(this.smallHash).notify(done);
+    });
+
+    it('should generate IPFS multihash of large data', function(done) {
+        expect(ipfsHash(this.large)).to.eventually.equal(this.largeHash).notify(done);
+    });
+});
+
+function setUp() {
+    this.small = 'banana slug';
+    this.smallHash = 'Qmcpsr5N2XwMtRWMsM7omHcFELtRETKTkdnvkB1CZqPmbw';
+    this.large = Buffer.alloc(1024**2, 'x').toString();
+    this.largeHash = 'QmZxqsmKZbwrPGAiPDgnUHKuh7FicKDFMpaE8B6mhdQAH3';
+    fs.writeFileSync('file.txt', this.large);
+}
