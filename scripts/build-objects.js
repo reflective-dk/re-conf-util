3892e405c6acd4bb7dba9c912a0c01d6f01012cf
Rasmus Borgsmidt <rasmus@borgsmidt.dk>
2018-10-08 14:15:00 +0200

    remove obsolete class view functionality

--- scripts/build-objects.js
+++ scripts/build-objects.js
@@ -6,7 +6,6 @@ var inspect = require('util').inspect;
 var fs = require('fs');
 var path = require('path');
 var buildObjects = require(path.join(__dirname, '../lib/build-objects'));
-var buildClassDiagram = require(path.join(__dirname, '../lib/build-class-diagram'));
 var conf = require(process.env.PWD); // Loads index.js of outer npm project
 
 buildObjects(conf)
@@ -14,11 +13,6 @@ buildObjects(conf)
         var output = JSON.stringify({ objects: objects }, null, 2);
         try { fs.mkdirSync('build'); } catch(e) {}
         fs.writeFileSync(path.join(process.env.PWD, 'build/objects.json'), output);
-        return buildClassDiagram(conf);
-    })
-    .then(function(classDiagram) {
-        try { fs.mkdirSync('build'); } catch(e) {}
-        fs.writeFileSync(path.join(process.env.PWD, 'build/class-diagram.html'), classDiagram);
     })
     .catch(function(errors) {
         console.log('Operation failed:');
