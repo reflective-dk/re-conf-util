d8af2e4ac931383c66dbe724810d5e23cc4b5cc7
Michael Nielsen <michael@brendborg.dk>
2019-03-31 11:43:40 +0000

    html and pdf docs based on configs

--- scripts/build-documents.js
+++ scripts/build-documents.js
@@ -5,32 +5,79 @@
 var inspect = require('util').inspect;
 var fs = require('fs');
 var path = require('path');
-var Remarkable = require("remarkable");
+var through = require('through');
 var Markdownpdf = require("markdown-pdf");
 var buildDocuments = require(path.join(__dirname, '../lib/build-documents'));
 var conf = require(process.env.PWD); // Loads index.js of outer npm project
 
+var preProcessHtml = function() {
+
+    return through(function(data) {
+    });
+};
+
 buildDocuments(conf)
     .then(function(docs) {
-//console.log("DEBUG: buildDocuments docs=", docs);
         let options = {
             remarkable: {
-                preset: true,
+                preset: 'full',
                 html: true,
                 linkify: true,
                 typographer: true,
             }
         };
+        let dstDir = path.join(process.env.PWD, 'build', 'docs')
 
-        let mdHtml = new Remarkable(options.remarkable);
-        let mdPdf = new Markdownpdf(options);
+//        let mdHtml = new Remarkable(options.remarkable);
+
+        if (!fs.existsSync(dstDir)) {
+            fs.mkdirSync(dstDir, {recursive: true});
+        }
 
         // Build each markdown file to HTML and PDF. Only docs in docs/ root are used
         if (docs) docs.forEach(doc => {
-            if (doc) Object.keys(doc).forEach(key => {
-console.log(key);
+            let src;
+            if (doc.startsWith(conf.name)) {
+                src = doc.replace(conf.name + path.sep, '');
+            } else {
+                doc = "re-" + doc;
+                src = path.join('node_modules',doc);
+            }
 
-            });
+            src = path.join(process.env.PWD, src);
+
+            let mdPdf = Markdownpdf(options);
+console.log("DEBUG: src=",src);
+console.log("DEBUG: option.pwd=",options.pwd);
+
+            // XXX: Move other doc types
+            if (doc.endsWith(".md")) {
+                doc = doc.replace('docs'+path.sep, '');
+
+                let dstPdf = path.join(dstDir, doc.replace('.md', '.pdf'));
+                let dstHtml = path.join(dstDir, doc.replace('.md', '.html'));
+
+                // replace image path to absolute
+//                const absolutePath = 'file:///' + process.cwd().replace(/ /g, '%20').replace(/\\/g, '/') + '/';
+//                const find = /img src="/g;
+//                const replace = 'img src="' + absolutePath;
+//                const mdStr = mdSourceStr.replace(find, replace);
+
+//console.log("DEBUG: preProcessHtml=",options.preProcessHtml);
+//options.preProcessHtml().pipe(process.stdout);
+//                options.preProcessHtml = function () { return through() };
+                mdPdf.from(src).to(dstPdf, function () {
+                  console.log(doc+" Done")
+                })
+
+
+//                    fs.createReadStream(src)
+//                    .pipe(mdPdf)
+//                    .pipe(fs.createWriteStream(dst))
+
+//.pipe(fs.createWriteStream('out.txt'))
+            }
+
         });
 
 
@@ -45,7 +92,3 @@ console.log(key);
         console.log('Operation failed:');
         console.log(inspect(errors, null, null));
     });
-
-function getDocument(path) {
-
-}
