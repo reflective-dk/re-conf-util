642dea8185b51088f9ad55d275628ab343490ad7
Rasmus Borgsmidt <rasmus@borgsmidt.dk>
2020-08-28 15:47:18 +0200

    robustness

--- scripts/list-integrations.js
+++ scripts/list-integrations.js
@@ -2,6 +2,7 @@
 
 "use strict";
 
+var _ = require('lodash');
 var path = require('path');
 var models = require('re-models').model;
 var buildObjects = require(path.join(__dirname, '../lib/build-objects'));
@@ -9,11 +10,11 @@ var conf = require(process.env.PWD); // Loads index.js of outer npm project
 
 buildObjects(conf).then(function(objects) {
     console.log('Integration Specs:');
-    objects.filter(o => o.registrations[0].validity[0].input.class.id ===
+    objects.filter(o => _.get(o, 'registrations[0].validity[0].input.class.id') ===
                    models.integration.classes['integration-spec'].id)
         .forEach(print);
     console.log('Extraction Specs:');
-    objects.filter(o => o.registrations[0].validity[0].input.class.id ===
+    objects.filter(o => _.get(o, 'registrations[0].validity[0].input.class.id') ===
                    models.integration.classes['extraction-spec'].id)
         .forEach(print);
 });
